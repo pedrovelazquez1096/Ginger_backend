2ce59d6cc7b1b3af02c603d1041c9fa3d020d021
pvelazquez1096 <86578496+pvelazquez1096@users.noreply.github.com>
2021-12-09 20:42:43 -0600

    First Commit

---
+++ src/main/java/com/pvelazquez/gingerIoT/GingerIoTApplication.java
@@ -0,0 +1,13 @@
+package com.pvelazquez.gingerIoT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GingerIoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GingerIoTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pvelazquez/gingerIoT/GingerIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvelazquez.gingerIoT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GingerIoTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
